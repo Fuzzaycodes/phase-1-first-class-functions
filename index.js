05728c3e8b737207c3cbb8fedc325dde1aeffe3f
Fuzzay <samomwerif2@gmail.com>
2024-07-08 14:34:14 +0300

    first-class-functions

---
+++ index.js
@@ -0,0 +1,20 @@
+function receivesAFunction(callback) {
+    callback(); 
+}
+
+
+function returnsANamedFunction() {
+    
+    function namedFunction() {
+        console.log("I am a named function!");
+    }
+    return namedFunction;
+}
+
+
+function returnsAnAnonymousFunction() {
+   
+    return () => {
+        console.log("I am an anonymous function!");
+    };
+}
